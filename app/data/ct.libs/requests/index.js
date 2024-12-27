f2db51a8fb7843e131ff6de5bf457f3c1a8cb272
Banbury <banbury20@gmail.com>
2024-12-27 09:10:51 +0100

    Made changes requested by reviewer

--- app/data/ct.libs/requests/index.js
+++ app/data/ct.libs/requests/index.js
@@ -1,28 +1,6 @@
-async function getRequest(url, headers = {}) {
-    return fetch(url, { headers: headers }).then(res => {
-        if (res.status == 200) {
-            const contentType = res.headers.get("content-type");
-            if (contentType || contentType.includes("application/json")) {
-                return res.json()
-            }
-            return res.text()
-        } else {
-            const msg = `Request failed with status ${res.status}`
-            console.error(msg)
-            throw new Error(msg)
-        }
-    })
-}
-
-async function postRequest(url, headers = {}, body) {
-    return fetch(url,
-        {
-            method: "POST",
-            body: JSON.stringify(body),
-            headers: headers
-        }
-    )
-        .then(res => {
+const requests = {
+    async getRequest(url, headers = {}) {
+        return fetch(url, { headers: headers }).then(res => {
             if (res.status == 200) {
                 const contentType = res.headers.get("content-type");
                 if (contentType || contentType.includes("application/json")) {
@@ -35,4 +13,28 @@ async function postRequest(url, headers = {}, body) {
                 throw new Error(msg)
             }
         })
+    },
+
+    async postRequest(url, headers = {}, body) {
+        return fetch(url,
+            {
+                method: "POST",
+                body: JSON.stringify(body),
+                headers: headers
+            }
+        )
+            .then(res => {
+                if (res.status == 200) {
+                    const contentType = res.headers.get("content-type");
+                    if (contentType || contentType.includes("application/json")) {
+                        return res.json()
+                    }
+                    return res.text()
+                } else {
+                    const msg = `Request failed with status ${res.status}`
+                    console.error(msg)
+                    throw new Error(msg)
+                }
+            })
+    }
 }
